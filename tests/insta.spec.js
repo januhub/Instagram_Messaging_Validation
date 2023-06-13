acadc922ba3e4599d2bbf49f1cbd56408e0c4fe1
Janani Elangovan <61153365+januhub@users.noreply.github.com>
2023-06-13 12:49:00 +0530

    Update insta.spec.js

--- tests/insta.spec.js
+++ tests/insta.spec.js
@@ -1,34 +1,32 @@
 import { test, expect } from '@playwright/test';
 
-test('Account 1: Janani', async ({ page }) => {
-  await page.goto('https://www.instagram.com/');
-  await page.getByLabel('Phone number, username, or email').click();
-  await page.getByLabel('Phone number, username, or email').fill('jan_96_jul');
-  await page.getByLabel('Phone number, username, or email').press('Tab');
-  await page.getByLabel('Password').fill('IAmJanani');
-  await page.getByLabel('Password').press('Enter');
-  await page.getByRole('button', { name: 'Not Now' }).click();
-  await page.getByRole('button', { name: 'Not Now' }).click();
-  await page.getByRole('link', { name: 'Direct messaging - 0 new notifications link' }).click();
-  await page.getByRole('button', { name: 'Send message' }).click();
-  await page.getByPlaceholder('Search...').fill('Jegan');
-  await page.getByRole('button', { name: 'je_gan_17 je_gan_17 Jegan Toggle selection' }).click();
-  await page.getByRole('button', { name: 'Chat' }).click();
-  await page.getByRole('paragraph').click();
-  await page.getByRole('textbox', { name: 'Message' }).fill('Hai Jegan');
-  await page.getByRole('textbox', { name: 'Message' }).press('Enter');
-  await page.getByRole('link', { name: 'Home' }).click();
+test('User1', async ({ page }) => {
+    await page.goto('https://www.instagram.com/');
+    await page.getByLabel('Phone number, username, or email').click();
+    await page.getByLabel('Phone number, username, or email').fill('jan_96_jul');
+    await page.getByLabel('Password').click();
+    await page.getByLabel('Password').fill('IAmJanani');
+    await page.getByRole('button', { name: 'Log in', exact: true }).click();
+    await page.getByRole('button', { name: 'Not Now' }).click();
+    await page.getByRole('button', { name: 'Not Now' }).click();
+    await page.getByRole('link', { name: 'Direct messaging - 0 new notifications link' }).click();
+    await page.getByRole('button', { name: 'User avatar Active Jegan Active now' }).click();
+    await page.getByRole('paragraph').click();
+    await page.getByRole('textbox', { name: 'Message' }).fill('Hai Jegan');
+    await page.getByRole('button', { name: 'Send' }).click();
 });
 
-test('Account 2: Jegan', async ({ page }) => {
-  await page.goto('https://www.instagram.com/');
-  await page.getByLabel('Phone number, username, or email').click();
-  await page.getByLabel('Phone number, username, or email').fill('je_gan_17');
-  await page.getByLabel('Password').click();
-  await page.getByLabel('Password').fill('IAmJegan');
-  await page.getByLabel('Password').press('Enter');
-  await page.getByRole('button', { name: 'Not Now' }).click();
-  await page.getByRole('button', { name: 'Not Now' }).click();
-  await page.getByRole('link', { name: 'Direct messaging - 1 new notification link' }).click();
-  await page.getByRole('link', { name: 'Home' }).click();
+
+test('User2', async ({ page }) => {
+    await page.goto('https://www.instagram.com/');
+    await page.getByLabel('Phone number, username, or email').click();
+    await page.getByLabel('Phone number, username, or email').fill('je_gan_17');
+    await page.getByLabel('Password').click();
+    await page.getByLabel('Password').fill('IAmJegan');
+    await page.getByRole('button', { name: 'Log in', exact: true }).click();
+    await page.getByRole('button', { name: 'Not Now' }).click();
+    await page.getByRole('button', { name: 'Not Now' }).click();
+    await page.getByRole('link', { name: 'Direct messaging - 1 new notification link' }).click();
+    await page.getByRole('button', { name: 'User avatar Active Janani Hai Jegan 1m' }).click();
+    await page.getByRole('link', { name: 'Home' }).click();
 });
